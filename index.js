87d9c8e9427013db0cc13652e41d8225e0f7c77c
Matthew Webb <mwebb2321@gmail.com>
2021-02-21 12:32:46 -0800

    questions should be done

--- index.js
+++ index.js
@@ -7,5 +7,41 @@ const questions = [
         message: "What is your project name?",
         name: "title"
     },
-    
+    {
+        type: "input",
+        message: "Write a short description of project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What command will install dependencies?",
+        name: "installation",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repository?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repository?",
+        name: "contribute"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests",
+        default: "npm run test"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
 ]
